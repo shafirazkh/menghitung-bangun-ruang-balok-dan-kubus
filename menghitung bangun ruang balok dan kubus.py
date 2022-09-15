84308194aa05b2268e068c373c3941e0967f14a0
shafirazkh <113223561+shafirazkh@users.noreply.github.com>
2022-09-15 22:11:13 +0700

    Add files via upload

---
+++ menghitung bangun ruang balok dan kubus.py
@@ -0,0 +1,13 @@
+# 1. volume Kubus 
+print("1. menghitung volume kubus")
+s = int(input('sisi:'))
+V = s*s*s
+print("jika sisi kubus adalah",s,"cm,","maka volume kubus adalah",V,"cm3.")
+
+# 2. volume balok
+print("2. menghitung volume balok")
+p = int(input('panjang balok:'))
+l = int(input('Lebar balok:'))
+t = int(input('tinggi balok:'))
+V = p*l*t
+print("jika panjang balok",p,"cm,","lebar balok",l,"cm,","tinggi balok",t,"cm,","maka volume balok adalah",V,"cm3.")
